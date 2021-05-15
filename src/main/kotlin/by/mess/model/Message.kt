e4bbf16d3e96fbb04777031c200c96f83a315065
Alexey Filinovich <aliaks35@gmail.com>
2021-05-15 10:09:03 +0300

    Refactoring of equals and hashCode

--- src/main/kotlin/by/mess/model/Message.kt
+++ src/main/kotlin/by/mess/model/Message.kt
@@ -17,13 +17,9 @@ data class Message(
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as Message
-        if (id != other.id) return false
-        return true
+        if (other !is Message) return false
+        return id == other.id
     }
 
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
+    override fun hashCode(): Int = id.hashCode()
 }
--- src/main/kotlin/by/mess/model/User.kt
+++ src/main/kotlin/by/mess/model/User.kt
@@ -12,13 +12,9 @@ data class User(
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        other as User
-        if (id != other.id) return false
-        return true
+        if (other !is User) return false
+        return id == other.id
     }
 
-    override fun hashCode(): Int {
-        return id.hashCode()
-    }
+    override fun hashCode(): Int = id.hashCode()
 }
