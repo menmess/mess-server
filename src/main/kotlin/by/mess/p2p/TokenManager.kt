2fc21c5b1970016ca4125df1fa49e231071e84a2
Alexey Filinovich <aliaks35@gmail.com>
2021-05-29 14:24:58 +0300

    Add tokens

---
+++ src/main/kotlin/by/mess/p2p/TokenManager.kt
@@ -0,0 +1,22 @@
+package by.mess.p2p
+
+import kotlinx.serialization.json.Json
+import java.util.*
+
+internal class TokenManager {
+    companion object {
+        @JvmStatic
+        fun getToken(peer: Peer): String {
+            val formatter = Json {}
+            val contents = formatter.encodeToString(Peer.serializer(), peer)
+            return Base64.getEncoder().encodeToString(contents.encodeToByteArray())
+        }
+
+        @JvmStatic
+        fun decodeToken(token: String): Peer {
+            val formatter = Json {}
+            val contents = String(Base64.getDecoder().decode(token))
+            return formatter.decodeFromString(Peer.serializer(), contents)
+        }
+    }
+}
