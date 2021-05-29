da053783b5b3ffd58ca4c9cd30db7b891520fa44
Elizabeth <52410353+elizabethfeden@users.noreply.github.com>
2021-05-29 16:09:36 +0300

    Draft listening to network

--- src/main/kotlin/by/mess/controller/Controller.kt
+++ src/main/kotlin/by/mess/controller/Controller.kt
@@ -7,7 +7,6 @@ import by.mess.model.Id
 import by.mess.model.Message
 import by.mess.model.MessageStatus
 import by.mess.model.User
-import by.mess.model.randomId
 import by.mess.p2p.DistributedNetwork
 import by.mess.storage.RAMStorage
 import by.mess.util.serialization.SerializerModule
@@ -17,6 +16,7 @@ import io.socket.client.Socket
 import io.socket.emitter.Emitter
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.runBlocking
 
 @ExperimentalCoroutinesApi
 class Controller(clientId: Id, app: Application) {
@@ -51,8 +51,16 @@ class Controller(clientId: Id, app: Application) {
 
     private val registerToken = Emitter.Listener {
         // @aleexf tbd
-        net.connect(it[0] as String)
-        net.eventBus.events.onEach { TODO("Handling network events") }
+        net.connect([0] as String)
+        net.eventBus.events.onEach { event -> when(event) {
+            is MessengerEvent.NewMessageEvent -> handleNewMessageEvent(event)
+            is MessengerEvent.ChangeMessageStatusEvent -> handleChangeMessageStatus(event)
+            is MessengerEvent.IntroductionRequest -> handleIntroductionRequest(event)
+            is MessengerEvent.IntroductionEvent -> handleIntroductionEvent(event)
+            is MessengerEvent.NewChatEvent -> handleNewChatEvent(event)
+            is MessengerEvent.NewChatRequest -> handleNewChatRequest(event)
+            // @aleexf add new user? remove him if he goes offline?
+        } }
     }
 
     private val registerUsername = Emitter.Listener {
@@ -66,14 +74,25 @@ class Controller(clientId: Id, app: Application) {
         storage.addNewMessage(message)
         // @uuustrica is it needed?
         // frontConnection.emit("send_message", it[0])
-        // @aleexf how to run suspend function?
-        net.eventBus.post(NetworkEvent.SendToPeerEvent(clientId, storage.getChat(message.chatId).getOther(clientId),
-            MessengerEvent.NewMessageEvent(clientId, message)))
+        runBlocking {
+            net.eventBus.post(
+                NetworkEvent.SendToPeerEvent(
+                    clientId, storage.getChat(message.chatId).getOther(clientId),
+                    MessengerEvent.NewMessageEvent(clientId, message)
+                )
+            )
+        }
     }
 
     private val createChat = Emitter.Listener {
-        net.eventBus.post(NetworkEvent.SendToPeerEvent(clientId, it[0] as Id,
-            MessengerEvent.NewChatRequest(clientId)))
+        runBlocking {
+            net.eventBus.post(
+                NetworkEvent.SendToPeerEvent(
+                    clientId, it[0] as Id,
+                    MessengerEvent.NewChatRequest(clientId)
+                )
+            )
+        }
     }
 
     private val changeChat = Emitter.Listener {
@@ -85,12 +104,58 @@ class Controller(clientId: Id, app: Application) {
 
     private val readMessages = Emitter.Listener {
         for (messageId in storage.getChat(it[0] as Id).messages) {
-            net.eventBus.post(
-                NetworkEvent.SendToPeerEvent(
-                    clientId, storage.getChat(it[0] as Id).getOther(clientId),
-                    MessengerEvent.ChangeMessageStatusEvent(clientId, messageId, MessageStatus.READ)
+            runBlocking {
+                net.eventBus.post(
+                    NetworkEvent.SendToPeerEvent(
+                        clientId, storage.getChat(it[0] as Id).getOther(clientId),
+                        MessengerEvent.ChangeMessageStatusEvent(clientId, messageId, MessageStatus.READ)
+                    )
                 )
-            )
+            }
+        }
+    }
+
+    private fun handleNewMessageEvent(event: MessengerEvent.NewMessageEvent) {
+        if (!storage.isMessagePresent(event.message.id)) {
+            storage.addNewMessage(event.message)
+            frontConnection.emit("receive_message", event.message)
+        }
+    }
+
+    private fun handleChangeMessageStatus(event: MessengerEvent.ChangeMessageStatusEvent) {
+        storage.getMessage(event.messageId).status = event.newStatus
+        if (event.newStatus == MessageStatus.READ) {
+            // @aleexf, @uuustrica backend reads every message separately, frontend reads all messages in chat,
+            // we need to unify it
+            frontConnection.emit("read_messages", storage.getMessage(event.messageId).chatId)
+        }
+    }
+
+    private fun handleIntroductionRequest(event: MessengerEvent.IntroductionRequest) {
+        // @aleexf ???
+    }
+
+    private fun handleIntroductionEvent(event: MessengerEvent.IntroductionEvent) {
+        // @allexf ???
+    }
+
+    private fun handleNewChatEvent(event: MessengerEvent.NewChatEvent) {
+        storage.addNewChat(event.chat)
+        frontConnection.emit("add_chat", formatter.encodeToString(Chat.serializer(), event.chat)) // @uuustrica args?
+    }
+
+    private fun handleNewChatRequest(event: MessengerEvent.NewChatRequest) {
+        try {
+            val chat = storage.getChatForUser(event.producerId)
+            runBlocking {
+                net.eventBus.post(
+                    NetworkEvent.SendToPeerEvent(
+                        storage.clientId, event.producerId,  MessengerEvent.NewChatEvent(storage.clientId, chat)
+                    )
+                )
+            }
+        } catch (e: NoSuchElementException) {
+            // tbd: sync chat creation
         }
     }
 }
