b52b9ca6032788aaf6baaae13e53fbb4e6da126b
Alexey Filinovich <aliaks35@gmail.com>
2021-06-01 00:24:42 +0300

    Remember client id

--- src/main/kotlin/by/mess/main.kt
+++ src/main/kotlin/by/mess/main.kt
@@ -1,12 +1,14 @@
 package by.mess
 
 import by.mess.controller.Controller
+import by.mess.model.Id
 import by.mess.model.randomId
 import io.ktor.application.*
 import io.ktor.http.content.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import java.io.File
 
 @ExperimentalCoroutinesApi
 fun Application.backendModule() {
@@ -14,7 +16,14 @@ fun Application.backendModule() {
         pingPeriodMillis = environment.config.property("ktor.websocket.ping_period_ms").getString().toLong()
         timeoutMillis = environment.config.property("ktor.websocket.timeout_ms").getString().toLong()
     }
-    Controller(randomId(), this)
+    var clientId: Id = try {
+        File("id").readText().toLong()
+    } catch (e: Exception) {
+        randomId().also { File("id").writeText("$it") }
+    }
+    println("ClientId = $clientId")
+
+    Controller(clientId, this)
 }
 
 fun Application.frontendModule() {
